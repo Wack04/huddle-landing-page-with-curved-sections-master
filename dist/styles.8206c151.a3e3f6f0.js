ec0ec181dff47251d422c653d1686cb82fd53e2d
Akira Yagi <wackhunter928@gmail.com>
2021-11-01 16:13:18 +0530

    Phone Design Over at Least Half.

--- dist/styles.8206c151.a3e3f6f0.js
+++ dist/styles.8206c151.a3e3f6f0.js
@@ -184,7 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"sass/styles.scss":[function(require,module,exports) {
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"dist/styles.8206c151.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/styles.8206c151.js.map
+//# sourceMappingURL=/styles.8206c151.a3e3f6f0.js.map
